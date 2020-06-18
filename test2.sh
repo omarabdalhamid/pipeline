d831e005f057d37136bb65bb804c06dc759ab12b
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-06-18 14:49:59 +0200

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -5,7 +5,7 @@ cd  /home/gitlab-runner/pipeline/branch/$CI_COMMIT_SHORT_SHA
 git clone https://ibnabbas:5C_3Fk1MWCXsrry8djD6@gitlab.com/ibnabbas/ibn-abbas.git --branch $CI_COMMIT_REF_NAME
 cd $CI_PROJECT_NAME
 remote_master_sha=$(git ls-remote https://ibnabbas:5C_3Fk1MWCXsrry8djD6@gitlab.com/ibnabbas/ibn-abbas.git HEAD | awk '{ print $1}')
-local_master_sha=$(git rev-parse HEAD)
+local_master_sha=$(git rev-parse master)
 echo $remote_master_sha
 echo $local_master_sha
 
