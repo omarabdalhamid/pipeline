a2621317b8c7d6a51cd14eb6f9c2c99a4f509e34
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-07-01 07:30:02 +0200

    Update unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -1,4 +1,4 @@
 
-nohup docker exec -t bb3abb9e4e_odoo13_1 bash -c "odoo -d unit_test2 --db_host  db -w odoo --test-enable --test-tags  'standard,test_account_asset'" > unit_test.log  & timeout 720s tail -f unit_test.log 
+nohup docker exec -t bb3abb9e4e_odoo13_1 bash -c "odoo -d unit_test2 --db_host  db -w odoo --test-enable --test-tags  'standard,test_account_asset'" > unit_test.log  & timeout 180s tail -f unit_test.log 
 
 kill -9 $(ps aux |grep bb3abb9e4e_odoo13_1)
