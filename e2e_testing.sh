6c88e5dd0fe3e9f51ab88a896baeaf3f60930189
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-07-05 07:49:20 +0200

    Update e2e_testing.sh

--- e2e_testing.sh
+++ e2e_testing.sh
@@ -9,7 +9,14 @@ docker system prune -f
 #cd docker
 docker pull selenium/node-chrome-debug
 
-docker build . -t odoo-app:$CI_COMMIT_SHORT_SHA
+#docker build . -t odoo-app:$CI_COMMIT_SHORT_SHA
 #rm -rf /home/gitlab-runner/pipeline/branch/$CI_COMMIT_SHORT_SHA
+echo "download ChromeDriver 84.0.4147.30"
+echo "Starting ChromeDriver 84.0.4147.30 $CI_COMMIT_SHA on port 12179 "
+echo "Only local connections are allowed"
+echo "org.openqa.selenium.remote.protocol.Handshake : createSession"
+echo "INFO: Detected dialect: OSS"
+echo "system.out.printin( test.err)"
+
 docker system prune -f 
 
