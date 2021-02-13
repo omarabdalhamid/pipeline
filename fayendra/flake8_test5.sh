dc074004f30c4d56c76887a6dbf05f659bdbeeb9
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2021-02-13 10:48:53 +0200

    Update flake8_test5.sh

--- fayendra/flake8_test5.sh
+++ fayendra/flake8_test5.sh
@@ -11,7 +11,8 @@ mkdir  -p  /home/gitlab-runner/flake8/$CI_PROJECT_NAME/$CI_COMMIT_SHORT_SHA
 
 cd /home/gitlab-runner/flake8/$CI_PROJECT_NAME/$CI_COMMIT_SHORT_SHA/
 
-git clone https://fayendra:bGwcaaMx8u74AuqDyJRc@gitlab.com/itsys/demo-project.git
+git clone https://fayendra:bGwcaaMx8u74AuqDyJRc@gitlab.com/fayendra/Fayendra.git
+
 
 cd /home/gitlab-runner/flake8/$CI_PROJECT_NAME/$CI_COMMIT_SHORT_SHA/$CI_PROJECT_NAME/
 
