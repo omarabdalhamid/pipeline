dca506617aad5f8977b910dd570a9d7f279835f7
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2021-02-13 16:22:40 +0200

    Update check_master.sh

--- fayendra/check_master.sh
+++ fayendra/check_master.sh
@@ -2,11 +2,11 @@
 rm -rf /home/gitlab-runner/pipeline/branch/$CI_COMMIT_SHORT_SHA
 mkdir /home/gitlab-runner/pipeline/branch/$CI_COMMIT_SHORT_SHA
 cd  /home/gitlab-runner/pipeline/branch/$CI_COMMIT_SHORT_SHA
-git clone https://fayendra:bGwcaaMx8u74AuqDyJRc@gitlab.com/itsys/demo-project.git 
+git clone https://fayendra:bGwcaaMx8u74AuqDyJRc@gitlab.com/fayendra/Fayendra.git 
 cd $CI_PROJECT_NAME
 git checkout $CI_COMMIT_REF_NAME
 git rev-list HEAD > check_master.txt 
-remote_master_sha=$(git ls-remote https://fayendra:bGwcaaMx8u74AuqDyJRc@gitlab.com/itsys/demo-project.git HEAD | awk '{ print $1}')
+remote_master_sha=$(git ls-remote https://fayendra:bGwcaaMx8u74AuqDyJRc@gitlab.com/fayendra/Fayendra.git HEAD | awk '{ print $1}')
 
 
 if grep -Fxq "$remote_master_sha"  ./check_master.txt
